80cc808f63eb60326a90ed6017321bf261d02249
malingahasantha <malinga.hasantha@gmail.com>
2024-07-24 08:12:54 +0530

    Create S3 Bucket

---
+++ s3_bucket/s3.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "my-tf-demo-bucket"
+
+  tags = {
+    Name = "My Demo Bucket"
+  }
+
+}
+
+/* bucket - (Optional, Forces new resource) Name of the bucket. 
+If omitted, Terraform will assign a random, unique name. 
+Must be lowercase and less than or equal to 63 characters in length. 
+The name must not be in the format "[bucket_name]--[azid]--x-s3"  */
+
+
+/* Versioning is a means of keeping multiple variants of an object in the same bucket. 
+You can use versioning to preserve, retrieve, and restore every version of every object stored in your Amazon S3 bucket. 
+With versioning, you can easily recover from both unintended user actions and application failures. */
