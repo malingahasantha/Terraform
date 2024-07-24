c2b85cfc4929c0d7141a145e825cc41b8feb3fc2
malingahasantha <malinga.hasantha@gmail.com>
2024-07-24 09:01:08 +0530

    Uploading file to S3 bucket_update the source path

--- s3_bucket/s3.tf
+++ s3_bucket/s3.tf
@@ -15,12 +15,6 @@ resource "aws_s3_bucket" "s3_bucket" {
 
 }
 
-resource "aws_s3_bucket_object" "hello_file" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  key = "hello.txt"
-  source = "D:\-- TUTORIALS --\Terraform\s3_bucket\hello.txt"
-}
-
 /* bucket - (Optional, Forces new resource) Name of the bucket. 
 If omitted, Terraform will assign a random, unique name. 
 Must be lowercase and less than or equal to 63 characters in length. 
@@ -30,3 +24,14 @@ The name must not be in the format "[bucket_name]--[azid]--x-s3"  */
 /* Versioning is a means of keeping multiple variants of an object in the same bucket. 
 You can use versioning to preserve, retrieve, and restore every version of every object stored in your Amazon S3 bucket. 
 With versioning, you can easily recover from both unintended user actions and application failures. */
+
+resource "aws_s3_bucket_object" "hello_file" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  key = "hello.txt"
+  source = "D:/-- TUTORIALS --/Terraform/s3_bucket/hello.txt"
+}
+
+# Uploading file to S3 Bucket
+/* bucket = aws_s3_bucket.s3_bucket.id - declare the s3 bucket we created above
+key = hello.txt - is the name of the file we are uploading
+source = "" - is the file path */
